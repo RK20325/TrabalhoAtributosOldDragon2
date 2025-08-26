4f1a73217704dd6a403ad83d13ed98ab47ecc3e5
RK20325 <renankusdra@gmail.com>
2025-08-26 14:11:11 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -6,12 +6,12 @@ class ClassePersonagem:
 
 class Guerreiro(ClassePersonagem):
     def __init__(self):
-        super().__init__(nome="Guerreiro", dado_vida="1d8", habilidades=["Ataque poderoso", "Armadura pesada"])
+        super().__init__(nome="Guerreiro", dado_vida="1d8", habilidades=["Lamina parte Serpentes", "Rigidez Voraz"])
 
 class Mago(ClassePersonagem):
     def __init__(self):
-        super().__init__(nome="Mago", dado_vida="1d4", habilidades=["Magia arcana", "Conhecimento"])
+        super().__init__(nome="Mago", dado_vida="1d4", habilidades=["Cometa Cintilante", "Esfera da Verdade"])
 
 class Ladino(ClassePersonagem):
     def __init__(self):
-        super().__init__(nome="Ladino", dado_vida="1d6", habilidades=["Furtividade", "Ataque furtivo"])
+        super().__init__(nome="Ladino", dado_vida="1d6", habilidades=["Furtividade espectral", "Estocada das Sombras"])
